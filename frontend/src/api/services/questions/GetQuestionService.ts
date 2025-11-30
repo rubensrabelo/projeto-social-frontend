554374dab67a58b9b86152e99a1a516664a69711
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-11-30 19:55:32 -0300

    feat: adiciona o service para pegar uma questão

---
+++ frontend/src/api/services/questions/GetQuestionService.ts
@@ -0,0 +1,26 @@
+import ENV from "../../../config/envConfig.ts";
+import { AuthError } from "../../errors/AuthError.ts";
+import { parseErrorResponse } from "../../utils/parseErrorResponse.ts";
+
+export async function GetQuestionService(
+    id_teacher: string,
+    id_bank: number,
+    id_question: number,
+) {
+    const url = `${ENV.API_BASE_URL}/professores/${id_teacher}/banco_questoes/${id_bank}/questoes/${id_question}`;
+
+    const response = await fetch(url, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+        const { message, status } = await parseErrorResponse(response);
+        throw new AuthError(message, status);
+    }
+
+    const data = await response.json();
+
+    return data["questao"] || {};
+}
+
