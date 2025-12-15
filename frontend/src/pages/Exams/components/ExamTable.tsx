bd926e9143cc36523256a4732f6cc20f346742ec
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-12-15 08:40:20 -0300

    refactor: formata a data para pt-br em exames

--- frontend/src/pages/Exams/components/ExamTable.tsx
+++ frontend/src/pages/Exams/components/ExamTable.tsx
@@ -8,6 +8,11 @@ interface Props {
 }
 
 export default function ExamTable({ exams, handleDelete, startEdit }: Props) {
+  function formatDateToPtBr(date: string): string {
+    const [year, month, day] = date.split("-");
+    return `${day}/${month}/${year}`;
+  }
+
   return (
     <div className={styles.tableWrapper}>
       <table className={styles.table}>
@@ -17,7 +22,7 @@ export default function ExamTable({ exams, handleDelete, startEdit }: Props) {
             <th>Área</th>
             <th>Qtde Questões</th>
             <th>Bimestre</th>
-            <th>Data de Realização</th>
+            <th>Data e hora de Realização</th>
             <th>Ações</th>
           </tr>
         </thead>
@@ -28,7 +33,7 @@ export default function ExamTable({ exams, handleDelete, startEdit }: Props) {
               <td>{e.area}</td>
               <td>{e.quantidade_questoes}</td>
               <td>{e.bimestre}</td>
-              <td>{e.dia_a_ser_realizada}</td>
+              <td>{formatDateToPtBr(e.dia_a_ser_realizada)} {e.hora_a_ser_liberada}</td>
               <td>
                 <button className={styles.editBtn} onClick={() => startEdit(e)}>Editar</button>
                 <button className={styles.deleteBtn} onClick={() => handleDelete(e.id!)}>Excluir</button>
